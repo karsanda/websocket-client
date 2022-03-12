d6703ef3ca05940866fb0f6a0dd6c7289a1866d8
Pinto Karsanda <pintokarsanda@gmail.com>
2022-03-12 13:31:25 +0700

    Install socketto

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+import { openConnection } from 'socketto'
+
+let ws
 
 function App() {
+  const [value, setValue] = useState('')
+  const [output, setOutput] = useState([])
+
+  function handleChange(e) {
+    setValue(e.target.value)
+  }
+
+  function printOutput(message) {
+    return setOutput((oldOutput) => [...oldOutput, message])
+  }
+
+  function open() {
+    ws = openConnection('ws://localhost:8080/connect', {
+      onOpen: () => printOutput('OPEN'),
+      onClose: () => printOutput('CLOSE'),
+      onMessage: (e) => printOutput(`RESPONSE: ${e.data}`),
+      onError: (e) => printOutput(`ERROR: ${e.data}`)
+    })
+  }
+
+  function close() {
+    ws.close()
+  }
+
+  function send() {
+    printOutput(`SEND: ${value}`)
+    ws.sendMessage(value)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <>
+      <div>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Click "Open" to create a connection to the server,
+          "Send" to send a message to the server and "Close" to close the connection.
+          You can change the message and send multiple times.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+        <div>
+          <button id="open" onClick={open}>Open</button>
+          <button id="close" onClick={close}>Close</button>
+        </div>
+        <input id="input" type="text" placeholder="Hello world!" onChange={handleChange} />
+        <button id="send" onClick={send}>Send</button>
+      </div>
+      <div id="output" style={{ maxHeight: '70vh', overflowY: 'scroll' }}>
+        {output.map(element => <p key={Math.random()}>{element}</p>)}
+      </div>
+    </>
+  )
 }
 
-export default App;
+export default App
